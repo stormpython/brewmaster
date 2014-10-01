1f168559fd766b4d8926d6289529f9e6755007c3
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-10-01 16:15:55 +0300

    adding brewmaster folder and api class

---
+++ brewmaster/API/__init__.py
@@ -0,0 +1,30 @@
+import requests
+import os
+
+
+class API:
+    """API Base Class"""
+    def __init__(self, url, key):
+        self.url = url
+        self.__key = key
+
+    def call_api(self, endpoint, params):
+        api_endpoint = self.url + '/' + endpoint
+        params['key'] = self.__key
+
+        results = requests.get(api_endpoint, params=params)
+
+        if results.status_code == 200:
+            return results
+        else:
+            # TODO: need a better solution here
+            print 'There was an error'
+
+
+class BreweryDbAPI(API):
+    """BreweryDB API Class"""
+    def __init__(self):
+        url = 'http://api.brewerydb.com/v2'
+        key = os.environ.get('BREWERY_DB_API_KEY')
+
+        API.__init__(self, url, key)
