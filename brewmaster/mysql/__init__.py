2c84c0fd485c9a98336c0eb0184eaa9799113753
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-10-01 16:41:26 +0300

    adding mysql class for database connections

---
+++ brewmaster/mysql/__init__.py
@@ -0,0 +1,54 @@
+from __future__ import absolute_import
+import pymysql
+
+from flask import _app_ctx_stack
+
+
+class MySQL(object):
+    def __init__(self, app=None):
+        if app is not None:
+            self.app = app
+            self.init_app(self.app)
+        else:
+            self.app = None
+
+    def init_app(self, app):
+        self.app = app
+        self.app.config.setdefault('MYSQL_DATABASE_HOST', 'localhost')
+        self.app.config.setdefault('MYSQL_DATABASE_PORT', 3306)
+        self.app.config.setdefault('MYSQL_DATABASE_USER', None)
+        self.app.config.setdefault('MYSQL_DATABASE_PASSWORD', None)
+        self.app.config.setdefault('MYSQL_DATABASE_DB', None)
+        self.app.config.setdefault('MYSQL_DATABASE_CHARSET', 'utf8')
+        self.app.teardown_request(self.teardown_request)
+        self.app.before_request(self.before_request)
+
+    def connect(self):
+        kwargs = {}
+        if self.app.config['MYSQL_DATABASE_HOST']:
+            kwargs['host'] = self.app.config['MYSQL_DATABASE_HOST']
+        if self.app.config['MYSQL_DATABASE_PORT']:
+            kwargs['port'] = self.app.config['MYSQL_DATABASE_PORT']
+        if self.app.config['MYSQL_DATABASE_USER']:
+            kwargs['user'] = self.app.config['MYSQL_DATABASE_USER']
+        if self.app.config['MYSQL_DATABASE_PASSWORD']:
+            kwargs['passwd'] = self.app.config['MYSQL_DATABASE_PASSWORD']
+        if self.app.config['MYSQL_DATABASE_DB']:
+            kwargs['db'] = self.app.config['MYSQL_DATABASE_DB']
+        if self.app.config['MYSQL_DATABASE_CHARSET']:
+            kwargs['charset'] = self.app.config['MYSQL_DATABASE_CHARSET']
+        return pymysql.connect(**kwargs)
+
+    def before_request(self):
+        ctx = _app_ctx_stack.top
+        ctx.pymysql = self.connect()
+
+    def teardown_request(self, exception):
+        ctx = _app_ctx_stack.top
+        if hasattr(ctx, "pymysql"):
+            ctx.pymysql.close()
+
+    def get_db(self):
+        ctx = _app_ctx_stack.top
+        if ctx is not None:
+            return ctx.pymysql
