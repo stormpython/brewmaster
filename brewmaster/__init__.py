e9f751d1086c553bce256f5c25ce28a71502980d
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-10-01 22:11:51 +0300

    making changes to brewmaster algorithm pseudocode

--- brewmaster/__init__.py
+++ brewmaster/__init__.py
@@ -1,19 +1,19 @@
 """
 Algorithm for brewmaster
 
-1. User inputs beer name into search box and clicks search.
+1. Accepts a beer name as input.
 2. The beer name is used to query the beers database.
 
-**If the beer and its list of similar beers is already stored in the database**
-    3. If the beer is in the database, the beer id is used to query the similar_beers database.
+**If the beer is already stored in the database**
+    3. The beer id is used to query the similar_beers database.
     4. The list of similar beers are returned to the user in order of similarity based on rank.
 
-**If data is not in database**
-    5. Else, an api call is made to obtain the relevant information for the beer.
+**If beer is not in database**
+    5. An api call is made to obtain the relevant information for the beer.
     6. The results of the api call are saved in the beers database.
     7. The characteristics of the beer are then used to obtain a list of beers with similar characteristics
-        thru another api call.
-    8. The beer name + its characteristics the user entered and the list of similar beers + their characteristics
+        through another api call.
+    8. The beer name the user entered + its characteristics and the list of similar beers + their characteristics
         returned from the api call are run through the algorithm to determine the rank order.
     9. The list of similar beers, characteristics, and rank are all saved in the similar_beers database.
     10. The results of the algorithm are returned to the user in rank order.
