a1a6a59ca47e7ac0eba1b6d7aa336b5576df19c5
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-10-08 00:22:51 +0300

    correcting mistakes in schemas

--- schema.sql
+++ schema.sql
@@ -3,17 +3,63 @@
 -- `mysql -u username -p database < schema.sql` from the project directory
 -- where username is your MySQL username
 
+CREATE TABLE IF NOT EXISTS glassware (
+  id TINYINT UNSIGNED NOT NULL,
+  name TINYTEXT NULL,
+  create_date DATETIME NULL,
+
+  PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = "utf8";
+
+CREATE TABLE IF NOT EXISTS categories (
+  id TINYINT UNSIGNED NOT NULL,
+  name TINYTEXT NULL,
+  create_date DATETIME NULL,
+
+  PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = "utf8";
+
+CREATE TABLE IF NOT EXISTS availability (
+  id TINYINT UNSIGNED NOT NULL,
+  name TINYTEXT NULL,
+  create_date DATETIME NULL,
+
+  PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = "utf8";
+
+CREATE TABLE IF NOT EXISTS styles (
+  id TINYINT UNSIGNED NOT NULL,
+  name TINYTEXT NULL,
+  description TEXT NULL,
+  category_id TINYINT UNSIGNED NULL,
+  ibu_min TINYINT UNSIGNED NULL,
+  ibu_max TINYINT UNSIGNED NULL,
+  abv_min DECIMAL(3, 1) UNSIGNED NULL,
+  abv_max DECIMAL(3, 1) UNSIGNED NULL,
+  srm_min TINYINT UNSIGNED NULL,
+  srm_max TINYINT UNSIGNED NULL,
+  og_min DECIMAL(4, 3) UNSIGNED NULL,
+  og_max DECIMAL(4, 3) UNSIGNED NULL,
+  fg_min DECIMAL(4, 3) UNSIGNED NULL,
+  fg_max DECIMAL(4, 3) UNSIGNED NULL,
+  create_date DATETIME NULL,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+) ENGINE = InnoDB DEFAULT CHARSET = "utf8";
+
+
 CREATE TABLE IF NOT EXISTS beer (
   id CHAR(6) NOT NULL,
   name TINYTEXT NULL,
   description TEXT NULL,
-  abv DECIMAL(2, 1) UNSIGNED NULL,
+  abv DECIMAL(3, 1) UNSIGNED NULL,
   ibu TINYINT UNSIGNED NULL,
   glassware_id TINYINT UNSIGNED NULL,
   style_id TINYINT UNSIGNED NULL,
   is_organic CHAR(1) NULL,
   food_pairings TEXT NULL,
-  original_gravity DECIMAL(1, 3) UNSIGNED NULL,
+  original_gravity DECIMAL(4, 3) UNSIGNED NULL,
   labels_icon TINYTEXT NULL,
   labels_medium TINYTEXT NULL,
   labels_large TINYTEXT NULL,
@@ -27,53 +73,8 @@ CREATE TABLE IF NOT EXISTS beer (
   create_date DATETIME NULL,
   update_date DATETIME NULL,
 
-  PRIMARY KEY ("id"),
-  FOREIGN KEY ("glassware_id") REFERENCES glassware("id"),
-  FOREIGN KEY ("style_id") REFERENCES styles("id"),
-  FOREIGN KEY ("available_id") REFERENCES availability("id")
-) ENGINE = InnoDB DEFAULT CHARSET = "utf8";
-
-CREATE TABLE IF NOT EXISTS glassware (
-  id TINYINT UNSIGNED NOT NULL,
-  name TINYTEXT NULL,
-  create_date DATETIME NULL,
-
-  PRIMARY KEY ("id")
-) ENGINE = InnoDB DEFAULT CHARSET = "utf8";
-
-CREATE TABLE IF NOT EXISTS styles (
-  id TINYINT UNSIGNED NOT NULL,
-  name TINYTEXT NULL,
-  description TEXT NULL,
-  category_id TINYINT UNSIGNED NULL,
-  ibu_min TINYINT UNSIGNED NULL,
-  ibu_max TINYINT UNSIGNED NULL,
-  abv_min DECIMAL(2, 1) UNSIGNED NULL,
-  abv_max DECIMAL(2, 1) UNSIGNED NULL,
-  srm_min TINYINT UNSIGNED NULL,
-  srm_max TINYINT UNSIGNED NULL,
-  og_min DECIMAL(1, 3) UNSIGNED NULL,
-  og_max DECIMAL(1, 3) UNSIGNED NULL,
-  fg_min DECIMAL(1, 3) UNSIGNED NULL,
-  fg_max DECIMAL(1, 3) UNSIGNED NULL,
-  create_date DATETIME NULL,
-
-  PRIMARY KEY ("id"),
-  FOREIGN KEY ("category_id") REFERENCES categories("id")
-) ENGINE = InnoDB DEFAULT CHARSET = "utf8";
-
-CREATE TABLE IF NOT EXISTS categories (
-  id TINYINT UNSIGNED NOT NULL,
-  name TINYTEXT NULL,
-  create_date DATETIME NULL,
-
-  PRIMARY KEY ("id")
-) ENGINE = InnoDB DEFAULT CHARSET = "utf8";
-
-CREATE TABLE IF NOT EXISTS availability (
-  id TINYINT UNSIGNED NOT NULL,
-  name TINYTEXT NULL,
-  create_date DATETIME NULL,
-
-  PRIMARY KEY ("id")
+  PRIMARY KEY (id),
+  FOREIGN KEY (glassware_id) REFERENCES glassware(id),
+  FOREIGN KEY (style_id) REFERENCES styles(id),
+  FOREIGN KEY (available_id) REFERENCES availability(id)
 ) ENGINE = InnoDB DEFAULT CHARSET = "utf8";
