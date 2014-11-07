292099d14a0f3ebd4087fd3e40245619f8505ba9
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-11-07 15:55:46 +0200

    improving algorithm comments for brew master

--- brewmaster/__init__.py
+++ brewmaster/__init__.py
@@ -1,20 +1,31 @@
-from brew_master import BrewMaster
-
 # Main Algorithm for Brew Master
 #
-# 1. Accepts a search term, which is either a beer or brand, name as input.
-# 2. The search term is looked up in the beers database.
-# 3. If the beer is found in the database, returns a dict of beer attributes.
-# 4. Else, makes an API call to the BreweryDB beers endpoint.
-# 5. If the beer is found in the API database, returns a dict of beer
-#    attributes.
-# 6. Save beer into the database.
-# 7. The style_id and abv beer attributes are extracted from the beer dict.
-# 8. An API call is made to the beers endpoint with style_id and abv values to
-#    obtain a list of similar beers. This list is returned as an list of dicts
-#    for each beer.
-# 9. If the search term is not found, an API call is made to the search
-#    endpoint.
-# 10. If results are returned, the list of dicts for each beer is returned.
-# 11. Else, if no results are returned, "search term could not be found" is
+# INPUT
+# 1. Accepts a search term as input, which is either a beer or brand name.
+#
+# GET BEER AND IT'S ATTRIBUTES
+# 2. The search term is looked up in the beer database.
+# 3. If the beer is found in the database, a dict of beer attributes is
 #    returned.
+# 4. Else, an API call is made to the BreweryDB API beer endpoint.
+# 5. If the beer is found in the API database, the beer is saved in the beer
+#    database, and a dict of beer attributes is returned.
+# ***IT'S IMPORTANT TO NOTE THAT ONLY 1 BEER SHOULD BE RETURNED HERE***
+#
+# GET LIST OF SIMILAR BEERS
+# 6. If a beer is found above, the style_id and abv attributes are extracted
+#    from the dictionary of beer attributes.
+# 7. An API call is made to the beers endpoint with the style_id and abv
+#    attributes as parameters to obtain a list of similar beers.
+#
+# SEARCH FOR BEER NAME OR BRAND NAME
+# 8. If a beer is not found in the first steps above, an API call is made to
+#    the BreweryDB API search endpoint.
+# 9. If there are any hits for the search term, a list of beers is returned.
+#
+# NO BEER OR BRAND NAME FOUND
+# 10. If still no beer(s) are found, then the user receives a `Beer not found`
+#    message.
+
+from brew_master import BrewMaster
+
