feb22dffce91bd8cba3fb89df37b2feb05e7384b
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-11-07 03:06:53 +0200

    cleaning up more code

--- brewmaster/brew_master.py
+++ brewmaster/brew_master.py
@@ -14,12 +14,17 @@ class BrewMaster:
         self.api_url = app.config["BREWERY_DB_API_URL"]
         self.api_key = app.config["BREWERY_DB_API_KEY"]
         self.brewery_db_api = API(self.api_url, self.api_key)
+        self.beer_endpoint = "beers"
+        self.search_endpoint = "search"
         self.api_call_limit = app.config["BREWERY_DB_API_LIMIT"]
         self.api_call_limit_message = app.config["API_LIMIT_REACHED_MESSAGE"]
 
     def db_lookup_search_term(self):
-        """Queries database for a specific beer by name, returns MySQL class
-         response"""
+        """Returns an array/list of dictionaries or 0.
+
+        Queries the MySQL beer table for the beer in the `search_term`.
+        Note: 0 == not in database
+        """
         query = """SELECT id, name, style_id, abv FROM beer WHERE name = %s"""
         args = (escape_string(self.search_term))
         return mysql.query(query, args)
@@ -71,66 +76,76 @@ class BrewMaster:
 
         mysql.query(query, args)
 
-    def call_beers_api_endpoint(self):
-        """Queries the BreweryDB beers endpoint for a specific beer. If the
-         beer is found, it saves the beer to the database and returns that
-         beer else returns not found response"""
-        endpoint = "beers"
-        params = {
-            "name": self.search_term
-        }
+    def call_api(self, endpoint, params):
+        """Returns an array/list of dictionaries, a not found string, or an
+        api limit string.
 
+        Accepts a endpoint string and a dictionary of parameters.
+
+        Makes an api call to the specified endpoint with the specified
+        parameters.
+        """
         if self.api_call_count < self.api_call_limit:
-            # Call API and save response
-            api_results = self.brewery_db_api.get(endpoint, params).json()
+            results = self.brewery_db_api.get(endpoint, params).json()
 
-            if 'totalResults' in api_results and api_results['totalResults'] == 1:
-                # If there is one beer in the api_results,
-                # return that beer object
-                beer = api_results['data'][0]
-
-                # Save beer to database
-                self.db_save_beer(beer)
-                return beer
+            if 'data' in results:
+                return results['data']
             else:
                 return self.not_found_response
         else:
             return self.api_call_limit_message
 
-    def get_beer(self):
-        """Does a look up for a specific beer and returns that beer if present,
-        else returns not found response"""
+    def call_beer_api_endpoint(self):
+        """Returns a JSON response for 1 beer, a not found string, or an api
+        limit string.
 
-        # Look up beer in database
+        This function should only return a dictionary for 1 beer.
+
+        If the API request limit has not been reached, an API call is made
+        to the BreweryDB `beers` endpoint using the `search_term`. If no
+        beer is found, a not found message is returned.
+        """
+        params = {"name": self.search_term}
+        result = self.call_api(self.beer_endpoint, params)
+
+        if isinstance(result, str):
+            return result
+        elif len(result) == 1:
+            return result[0]
+        else:
+            return self.not_found_response
+
+    def get_beer(self):
+        """Returns a dictionary, JSON response, or a not found string.
+
+        Queries the database using the `search_term` for a specific beer.
+        If the beer is not found, an API call is made. If a beer is still
+        not found, a not found response is returned.
+        """
         beer = self.db_lookup_search_term()
 
         if isinstance(beer, int) and beer == 0:
-            # if beer is not in the database:
-            # Make an API call
-            return self.call_beers_api_endpoint()
+            return self.call_beer_api_endpoint()
         elif len(beer) == 1:
-            # if 1 beer is in the database response
-            # Return the result from the database
             return beer[0]
         else:
-            # if response from database has more than 1 beer, return a not
-            #  found response. get_beer() should only return 1 beer
             return self.not_found_response
 
     def get_similar_beers(self, beer):
-        """Makes an API call to the BreweryDB beers endpoint based on the
-        `styleId` and `abv` if present. Returns a list of beers that match
-        the `styleId` and `abv`, else returns a not found response"""
-        endpoint = "beers"
-        style_id = int(beer["styleId"]) if 'styleId' in beer else None
+        """Returns an array/list, an api limit string, or a not found string.
 
-        # Calculate the abv range
+        Accepts a dictionary of beer attributes as input.
+
+        Makes an API call to the BreweryDB `beers` endpoint using the
+        `styleId` and `abv` attributes. The `styleId` attribute is
+        required.
+        """
+        style_id = int(beer["styleId"]) if 'styleId' in beer else None
         start_abv = int(float(beer["abv"])) if 'abv' in beer else None
         end_abv = start_abv + 1 if start_abv is not None else None
         abv_range = str(start_abv) + "," + str(end_abv) \
             if start_abv is not None else None
 
-        # Only make an API call if `styleId` is present
         if style_id is not None and abv_range is not None:
             params = {"styleId": style_id, "abv": abv_range}
         elif style_id is not None:
@@ -138,44 +153,23 @@ class BrewMaster:
         else:
             return self.not_found_response
 
-        if self.api_call_count < self.api_call_limit:
-            # Call API and save the results
-            results = self.brewery_db_api.get(endpoint, params).json()
-            if 'totalResults' in results and results['totalResults'] > 1:
-                # if results, return the data array
-                return results.json()['data']
-            else:
-                return self.not_found_response
-        else:
-            return self.api_call_limit_message
+        return self.call_api(self.beer_endpoint, params)
 
     def search(self):
         """Makes an API call to the BreweryDB search endpoint based on the
-        search_term. Returns a list of beers, else returns a not found
-         response"""
-        endpoint = "search"
+        search_term. Returns a list of beers, else returns a not found response
+        """
         params = {"q": self.search_term}
-
-        # Call API and save results
-        results = self.brewery_db_api.get(endpoint, params).json()
-        if 'data' in results:
-            # if data array present in results, return data array
-            return results['data']
-        else:
-            return self.not_found_response
+        return self.call_api(self.search_endpoint, params)
 
     def get_results(self):
         """Main algorithm for the BrewMaster class. It queries for a specific
         beer based on the `search_term` provided and returns a listing of
-        beers or a not found response"""
-
-        # Query for beer in database or API
+        beers or a not found response
+        """
         beer = self.get_beer()
-
         if beer == self.not_found_response:
-            # if no beer found, query for beer using the API search endpoint
             return self.search()
         else:
-            # if beer found, get list of similar beers
             return self.get_similar_beers(beer)
 
