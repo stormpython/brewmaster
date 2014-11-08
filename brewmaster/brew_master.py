27261518f97890c6f15b49b2265db124f95eeeb5
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-11-08 22:55:31 +0200

    fixing bugs

--- brewmaster/brew_master.py
+++ brewmaster/brew_master.py
@@ -7,14 +7,22 @@ import json
 
 class BrewMaster:
 
-    def __init__(self, search_term):
+    def __init__(self, search_term, is_id=False, page=1):
         self.search_term = search_term
+        self.is_id = is_id
+        self.page = page
+
+        # Database table
         self.db_table = "beers"
+
+        # API settings
         self.api_url = app.config["BREWERY_DB_API_URL"]
         self.api_key = app.config["BREWERY_DB_API_KEY"]
         self.brewery_db_api = API(self.api_url, self.api_key)
         self.beer_endpoint = "beers"
         self.search_endpoint = "search"
+
+        # Not found response
         self.not_found_response = app.config["NOT_FOUND_RESPONSE"]
 
     def db_lookup_search_term(self):
@@ -23,9 +31,14 @@ class BrewMaster:
         Queries the MySQL beer table for the beer in the `search_term`.
         Note: 0 means not in database
         """
-        query = """SELECT id, name, styleid, abv FROM beers WHERE name = %s"""
-        args = (escape_string(self.search_term))
-        return mysql.query(query, args)
+        attr = "id" if self.is_id is True else "name"
+        query = """
+                SELECT `id`, `name`, `styleid`, `abv`
+                FROM `beers`
+                WHERE `%s` = "%s"
+                """ % (attr, escape_string(self.search_term))
+
+        return mysql.query(query)
 
     @staticmethod
     def get_beer_kwargs(beer):
@@ -64,20 +77,21 @@ class BrewMaster:
         kwargs = self.get_beer_kwargs(beer)
 
         query = """
-                INSERT INTO beers (id, name, description, abv, ibu,
+                INSERT INTO `beers` (id, name, description, abv, ibu,
                     glasswareId, glass, styleId, style, isOrganic,
-                    foodPairings, originalGravity, labels, servingTemperature,
-                    servingTemperatureDisplay, status, statusDisplay,
-                    availableId, available, beerVariationId, beerVariation,
-                    year, createDate, updateDate)
+                    foodPairings, originalGravity, labels,
+                    servingTemperature, servingTemperatureDisplay,
+                    status, statusDisplay, availableId, available,
+                    beerVariationId, beerVariation, year, createDate,
+                    updateDate)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s,
                         %s, %s, %s, %s, %s, %s, %s, %s,
                         %s, %s, %s, %s, %s, %s, %s, %s);
                 """
         args = (
             kwargs["id"],
-            escape_string(kwargs["name"]),
-            escape_string(kwargs["description"]),
+            escape_string(kwargs["name"]) if kwargs["name"] is not None else None,
+            escape_string(kwargs["description"]) if kwargs["description"] is not None else None,
             kwargs["abv"],
             kwargs["ibu"],
             kwargs["glasswareId"],
@@ -101,8 +115,7 @@ class BrewMaster:
             kwargs["updateDate"]
         )
 
-        response = mysql.query(query, args)
-        print response
+        return mysql.query(query, args)
 
     def call_api(self, endpoint, params):
         """Returns an array/list of dictionaries, a not found string, or an
@@ -130,13 +143,19 @@ class BrewMaster:
         to the BreweryDB `beers` endpoint using the `search_term`. If no
         beer is found, a not found message is returned.
         """
-        params = {"name": self.search_term}
-        results = self.call_api(self.beer_endpoint, params)
+        if self.is_id is True:
+            endpoint = "beer/" + self.search_term
+            params = {}
+        else:
+            endpoint = self.beer_endpoint
+            params = {"name": self.search_term}
+
+        results = self.call_api(endpoint, params)
 
         if isinstance(results, str):
             return results
-        elif len(results) == 1:
-            beer = results[0]
+        elif len(results) == 1 or isinstance(results, dict):
+            beer = results[0] if len(results) == 1 else results
             self.db_save_beer(beer)
             return beer
         else:
@@ -158,7 +177,7 @@ class BrewMaster:
         else:
             return self.not_found_response
 
-    def get_similar_beers(self, beer):
+    def get_similar_beers(self, style_id, abv_range=None):
         """Returns an array/list, an api limit string, or a not found string.
 
         Accepts a dictionary of beer attributes as input.
@@ -167,26 +186,30 @@ class BrewMaster:
         `styleId` and `abv` attributes. The `styleId` attribute is
         required.
         """
-        if "styleid" in beer:
-            style_id = int(beer["styleid"])
-        elif "styleId" in beer:
-            style_id = int(beer["styleId"])
-        else:
-            style_id = None
-
-        start_abv = int(float(beer["abv"])) if "abv" in beer else None
-        end_abv = start_abv + 1 if start_abv is not None else None
-        abv_range = str(start_abv) + "," + str(end_abv) \
-            if start_abv is not None else None
+        params = {
+            "withBreweries": "Y",
+            "p": self.page
+        }
 
         if style_id is not None and abv_range is not None:
-            params = {"styleId": style_id, "abv": abv_range}
+            params["styleId"] = style_id
+            params["abv"] = abv_range
         elif style_id is not None:
-            params = {"styleId": style_id}
+            params["styleId"] = style_id
         else:
             return self.not_found_response
 
-        return self.call_api(self.beer_endpoint, params)
+        beers = self.call_api(self.beer_endpoint, params)
+
+        return {
+            "search_term": self.search_term,
+            "is_id": self.is_id,
+            "style_id": style_id,
+            "abv_range": abv_range,
+            "not_found": self.not_found_response,
+            "page": self.page,
+            "beers": beers
+        }
 
     def search(self):
         """Returns a JSON response or a not found string.
@@ -195,7 +218,15 @@ class BrewMaster:
         search_term.
         """
         params = {"q": self.search_term, "type": "beer"}
-        return self.call_api(self.search_endpoint, params)
+        beers = self.call_api(self.search_endpoint, params)
+
+        return {
+            "search_term": self.search_term,
+            "is_id": self.is_id,
+            "not_found": self.not_found_response,
+            "page": self.page,
+            "beers": beers
+        }
 
     def get_results(self):
         """Returns a JSON response or a not found string.
@@ -208,4 +239,15 @@ class BrewMaster:
         if beer == self.not_found_response:
             return self.search()
         else:
-            return self.get_similar_beers(beer)
+            if "styleid" in beer:
+                style_id = int(beer["styleid"])
+            elif "styleId" in beer:
+                style_id = int(beer["styleId"])
+            else:
+                style_id = None
+
+            start_abv = int(float(beer["abv"])) if "abv" in beer else None
+            end_abv = start_abv + 1 if start_abv is not None else None
+            abv_range = str(start_abv) + "," + str(end_abv) if start_abv is not None else None
+
+            return self.get_similar_beers(style_id, abv_range)
