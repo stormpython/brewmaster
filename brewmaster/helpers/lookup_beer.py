a99509915ccd33c0ba30ebd1ac2d9c9c75b79aa5
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-11-09 10:28:55 +0200

    removing indentation

--- brewmaster/helpers/lookup_beer.py
+++ brewmaster/helpers/lookup_beer.py
@@ -5,9 +5,9 @@ from app import mysql
 def lookup_beer(search_term, is_id):
     attr = "id" if is_id is True else "name"
     query = """
-                SELECT `id`, `name`, `styleid`, `abv`
-                FROM `beers`
-                WHERE `%s` = "%s"
-                """ % (attr, escape_string(search_term))
+            SELECT `id`, `name`, `styleid`, `abv`
+            FROM `beers`
+            WHERE `%s` = "%s"
+            """ % (attr, escape_string(search_term))
 
     return mysql.query(query)
