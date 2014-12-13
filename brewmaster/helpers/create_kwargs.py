7511b36270414eb7bb2cc11069518f824a2f74fa
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-12-13 22:17:24 +0100

    adding comments to helper functions

--- brewmaster/helpers/create_kwargs.py
+++ brewmaster/helpers/create_kwargs.py
@@ -1,8 +1,19 @@
+###############################################################################
+# This helper function creates a dictionary of keys and appropriately modified
+# MySQL responses as their values. Some MySQL responses are returned as
+# dictionaries, which need to be modified (json.dumps). Others are returned
+# as strings, which need escaping (escape_string). Finally, some values for
+# keys are not present and need to be given the special value `None`.
+###############################################################################
+
 import json
 from pymysql import escape_string
 
 
 def create_kwargs(key, iterator, kwargs):
+    """Callback function that returns a dictionary of keys with appropriately
+    modified value pairs.
+    """
     if key in iterator:
         value = iterator[key]
 
