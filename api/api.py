577d7e8f14d758660e358e1353a151310f8f4312
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-12-13 18:05:52 +0100

    updating comments in the api module

--- api/api.py
+++ api/api.py
@@ -3,14 +3,12 @@ import requests
 
 class API:
     """A base class for API requests"""
-
     def __init__(self, url, key):
         self.url = url
         self.__key = key
 
     def get(self, endpoint, params=None):
         """Makes an API request"""
-
         api_endpoint = self.url + '/' + endpoint
         params = {} if params is None else params
         params['key'] = self.__key
@@ -25,7 +23,6 @@ class API:
 
     def get_json(self, endpoint, params=None):
         """Makes an API request and returns a JSON formatted response"""
-
         results = self.get(endpoint, params)
 
         if results.raise_for_status() is None:
